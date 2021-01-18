c81f96f2f4719899916253dd483f48108f2f4dd2
Shahzadrori <shahzadrori@gmail.com>
2021-01-18 10:08:17 +0500

    init commit

---
+++ src/components/container.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Container =()=>{
+    return(
+        <>
+            <div className='wraper'>
+                <h1>WELCOME</h1>
+                <h4>Let us know what  you want to buy</h4>
+                <input type='text' value='' placeholder='ENTER THE ITEM' />
+                <button>Bought items</button>
+            </div>
+        </>
+    )
+}
+
+export default Container;
